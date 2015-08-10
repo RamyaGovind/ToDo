c121a6446e062c30fcbb376996f5ad97a1326645
Ramya <ramya@voonik.com>
2015-08-10 16:04:29 +0530

    tests

--- config/env/all.js
+++ config/env/all.js
@@ -6,7 +6,8 @@ var path = require('path'),
 module.exports = {
   root: rootPath,
   http: {
-    port: process.env.PORT || 3000
+    // port: process.env.PORT || 3000
+    port: 80
   },
   https: {
     port: false,
